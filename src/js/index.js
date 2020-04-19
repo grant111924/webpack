c8436cf9ac164b6e0f0ee6e74adf603d6631733d
Grant <grant111924@gmail.com>
2020-04-19 20:14:16 +0800

    src init

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import 'css/style.scss';
+import '../../node_modules/bootstrap/scss/bootstrap.scss';
+import $ from 'jquery';
+import 'popper.js';
+import 'bootstrap';
+
+$(()=>{
+    console.log("index");
+})
