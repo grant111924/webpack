44e2af12b3db8acafee2c945ac838ab18b3d4df7
Grant <grant111924@gmail.com>
2020-04-21 23:11:35 +0800

    custom bootstrap theme scss

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 import 'css/style.scss';
-import '../../node_modules/bootstrap/scss/bootstrap.scss';
+import 'css/custom.scss';
 import $ from 'jquery';
+
+// import '../../node_modules/bootstrap/scss/bootstrap.scss';
 import 'popper.js';
 import 'bootstrap';
 
